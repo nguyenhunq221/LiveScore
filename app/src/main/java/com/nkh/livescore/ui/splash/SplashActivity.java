934cd8ae48317048b378d41ec9e1ad79acc11d20
Hung Nguyen <nguyenhungbntb@gmail.com>
2023-05-09 23:52:39 +0700

    add splash

---
+++ app/src/main/java/com/nkh/livescore/ui/splash/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.nkh.livescore.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.nkh.livescore.R;
+import com.nkh.livescore.ui.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    SplashActivity.this.startActivity(mainIntent);
+                    SplashActivity.this.finish();
+            }
+        }, 4000);
+    }
+}
